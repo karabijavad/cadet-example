3309d6b3ad6e181080ad906edd80c9e6760e02db
Javad Karabi <karabijavad@gmail.com>
2014-02-15 14:36:08 -0500

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,7 @@ rm neo4j-community-2.0.1-unix.tar.gz
 
 cd data/
 git clone https://github.com/unitedstates/congress-data.git --depth=1
+git clone https://github.com/unitedstates/congress-legislators.git --depth=1
 
-mkdir congress/
-cd congress/
-
-mkdir 113/
-cd 113/
-wget http://unitedstates.sunlightfoundation.com/congress/data/113.zip
-unzip 113.zip
-
+mkdir .bundle/
+bundle install --path .bundle/
